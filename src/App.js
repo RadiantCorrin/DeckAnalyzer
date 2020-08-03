80d8837fe1a311ff40a125c8eb20c9af49878d48
CorbinM <modicc@uw.edu>
2020-08-03 01:23:31 -0700

    updated the list structure and added links to cards from the decklist

--- src/App.js
+++ src/App.js
@@ -12,34 +12,18 @@ export default class App extends React.Component {
   constructor(props) {
     super(props);
 
-    let testData = [
-      {
-        id: 3,
-        value: "nice"
-      }
-    ]
-
-    this.state = { loaded: false, deckURL: null, loadError: false, loadingDeck: false, testData: testData };
+    this.state = { loaded: false, deckURL: null, loadError: false, loadingDeck: false };
     this.loadDeck = this.loadDeck.bind(this);
     this.handleKeyPress = this.handleKeyPress.bind(this)
-
-    // const CustomBar = (props) => {
-    //   const {years, fill} = props;
-    //   //business logic here to update fill color explicitly
-    //   if(years === 'Current') {
-    //       fill='#NewFillColor';
-    //   }
-    
-    //   //use explicit fill here, or use the additional css class and make a css selector to update fill there
-    //   return <Rectangle {...props} fill={fill} className=`recharts-bar-rectangle ${years}` />
-    // };
   }
 
   render() {
     return (
       <div style={{ height: '100vh', width: "100vw" }}>
+        {/* The header for the website when nothing is loaded */}
         {!this.state.loaded && <Header />}
 
+        {/* The welcome page of the analyzer. */}
         {!this.state.loaded &&
           <Container fluid>
             <Card>
@@ -74,6 +58,7 @@ export default class App extends React.Component {
             </Card>
           </Container>}
 
+        {/* The information display for after the deck has been loaded / analyzed */}
         {this.state.loaded &&
           <div className="customContainer">
             <div className="header">
@@ -84,7 +69,12 @@ export default class App extends React.Component {
                 <CardHeader><b>Deck List and Price (TCG, CK)</b></CardHeader>
               </Card>
               <div style={{ display: 'block', maxHeight: "65%", overflow: 'auto' }}>
-                {this.state.listOfCards}
+                <table>
+                  <tbody>
+                    {this.state.listOfCards}
+                  </tbody>
+                </table>
+                
               </div>
               <Card style={{ maxHeight: "25%", overflow: 'auto' }}>
                 <p><b>Stats:</b></p>
@@ -144,7 +134,6 @@ export default class App extends React.Component {
                   <XAxis dataKey="color"></XAxis>
                   <YAxis></YAxis>
                   <Tooltip />
-                  {/* fill='#4b71db' */}
                   <Bar dataKey="number">
                   {
                     this.state.colorData.map((entry, index) => (
@@ -185,29 +174,31 @@ export default class App extends React.Component {
     );
   }
 
-  //style={{ width: 550, height: 370, minWidth: 550, minHeight: 360 }}
-
-  // Allows enter to submit the form, and prevents the page from refreshing.
+  /**
+   * A small helper method that stops the form from reloading the page and submitting in
+   * the normal way, and instead calls the native deck loading function
+   * @param {The action the user is triggering} event 
+   */
   handleKeyPress(event) {
     event.preventDefault()
     this.loadDeck()
   }
 
+  /**
+   * Loads the decklist from the URL the user provides, and does analysis of
+   * the deck for the other components to display.
+   */
   loadDeck() {
     let jsonDeck = null
 
-    // Read in the deck with a lookup, which is an Axios promise
-    // Gotta wait for it to be done to do anything
-
-    // console.log("The URL I got was:" + this.state.deckURL)
-
+    // If no valid url is found, don't try loading!
     if (this.state.deckURL == null || "") {
       console.log("Null/empty URL on call")
       return
     }
 
+    // Split out the parts of the URL that are separated by slashes
     let urlParts = this.state.deckURL.split("/")
-
     if (urlParts.length < 5) {
       console.log("Error parsing URL")
       return;
@@ -219,29 +210,26 @@ export default class App extends React.Component {
     // The unique ID for an archidekt deck. Needed for lookup
     let number = parseInt(nameAndNumberParts[0])
 
-    // console.log(urlParts)
-    // console.log(number)
-
-    // Start loading the deck
+    // Update state so we can start loading the deck
     this.setState({ loadingDeck: true })
 
-    // Fetch the deck, and start analysis
+    // Read in the deck with a lookup, which is an Axios promise
+    // Gotta wait for it to be done to do anything
     archidekt.fetchDeckById(number).then((response) => {
       jsonDeck = response
 
-      this.setState({ loadingDeck: true })
-
       // If there is some error fetching the deck, set the state and exit
       if (jsonDeck == null) {
-        this.setState({ loadError: true, loadDeck: false })
+        this.setState({ loadError: true, loadDeck: false, loadingDeck: false })
         this.console.log("Error loading deck!")
         return
       }
 
-      // Extract 
+      // Extract the actual json object that holds all the cards
       let jsonCards = jsonDeck.data.cards
       console.log(jsonCards)
 
+      // Set up variables to be used to track information about the cards that were loaded
       let jsCards = []
 
       let cmcRawData = {}
@@ -254,46 +242,36 @@ export default class App extends React.Component {
       let maxTCG = null
       let maxCK = null
 
+      // For each card, parse all the information we need from it
       for (let i = 0; i < jsonCards.length; i++) {
-        // TODO 
-        // Change the price from the prices array based on whether or not this card is foil :)
-
-        /**
-         * prices:
-          ck: 0.25
-          ckfoil: 0.99
-          cm: 0.11
-          cmfoil: 0.06
-          mtgo: 0.03
-          mtgofoil: 0.77
-          tcg: 0.12
-          tcgfoil: 0.43
-        */
-
         // Skip all cards in the MAYBEBOARD Category by default
         if (jsonCards[i].category === "Maybeboard") {
           continue
         }
 
+        // Get the price for TCGPlayer and CK, depending on wether the card is foil or not 
         let tmpTCGPrice = (jsonCards[i].modifier === "Foil") ? jsonCards[i].card.prices.tcgfoil : jsonCards[i].card.prices.tcg;
         let tmpCKPRice = (jsonCards[i].modifier === "Foil") ? jsonCards[i].card.prices.ckfoil : jsonCards[i].card.prices.ck;
 
+        // Increase the cost by the quantity
         tcgTotalCost += tmpTCGPrice * jsonCards[i].quantity
         ckTotalCost += tmpCKPRice * jsonCards[i].quantity
 
+        // Compare this card's price to the max we have seen so far, updating if needed
         // These could be merged, but I think it is more readable with seperate cases
         if (maxTCG == null) {
           maxTCG = { name: jsonCards[i].card.oracleCard.name, cost: tmpTCGPrice }
         } else if (maxTCG.cost < tmpTCGPrice) {
           maxTCG = { name: jsonCards[i].card.oracleCard.name, cost: tmpTCGPrice }
         }
-
+        // Same with CK
         if (maxCK == null) {
           maxCK = { name: jsonCards[i].card.oracleCard.name, cost: tmpCKPRice }
         } else if (maxCK.cost < tmpCKPRice) {
           maxCK = { name: jsonCards[i].card.oracleCard.name, cost: tmpCKPRice }
         }
 
+        // Create the custom JavaScript object to hold all of this card's data
         let tmp = new MTGCard(jsonCards[i].card.oracleCard.name,
           jsonCards[i].card.oracleCard.colors,
           jsonCards[i].card.oracleCard.manaCost,
@@ -302,7 +280,9 @@ export default class App extends React.Component {
           jsonCards[i].modifier,
           tmpTCGPrice,
           tmpCKPRice,
-          jsonCards[i].card.edition.editioncode);
+          jsonCards[i].card.edition.editioncode,
+          jsonCards[i].card.edition.editionname
+          );
 
 
         // Add the CMC of this card to the raw values for the deck
@@ -314,10 +294,7 @@ export default class App extends React.Component {
         }
 
         // Add the types of this card to the raw values of the deck
-        // TODO: if a card has more than 1 (aka, is a basic land or something) 
-        //       then we need to add that type TIMES the quanitity of the card
         for (let j = 0; j < tmp.types.length; j++) {
-          console.log(jsonCards[i].card.oracleCard.name + jsonCards[i].quantity)
           let type = tmp.types[j]
           if (type in typeRawData) {
             typeRawData[type] = typeRawData[type] + (1 * jsonCards[i].quantity)
@@ -326,7 +303,8 @@ export default class App extends React.Component {
           }
         }
 
-        // Add the colors of this card to the list
+        // TODO: Needs support for colorless cards
+        // Add the colors of this card to the tracker for those in the deck
         for (let j = 0; j < tmp.colors.length; j++) {
           let color = tmp.colors[j]
           if (color in colorRawData) {
@@ -336,56 +314,95 @@ export default class App extends React.Component {
           }
         }
 
-        // TODO: what is the character symbol for colorless?
-        // Count the actual symbols
+        // TODO: Needs support for colorless mana symbols
+        // Parse the pip color symbols for this card and add them to the trackers
         for (let j = 0; j < tmp.manaCost.length; j++) {
           let ch = tmp.manaCost.charAt(j)
 
-          if (ch === "W") {
-            if ("White" in pipsRawData) {
-              pipsRawData["White"]++
-            } else {
-              pipsRawData["White"] = 1
-            }
-          } else if (ch === "U") {
-            if ("Blue" in pipsRawData) {
-              pipsRawData["Blue"]++
-            } else {
-              pipsRawData["Blue"] = 1
-            }
-          } else if (ch === "B") {
-            if ("Black" in pipsRawData) {
-              pipsRawData["Black"]++
-            } else {
-              pipsRawData["Black"] = 1
-            }
-          } else if (ch === "R") {
-            if ("Red" in pipsRawData) {
-              pipsRawData["Red"]++
-            } else {
-              pipsRawData["Red"] = 1
-            }
-          } else if (ch === "G") {
-            if ("Green" in pipsRawData) {
-              pipsRawData["Green"]++
-            } else {
-              pipsRawData["Green"] = 1
-            }
+          // Match the manacost character to the name of the color it belongs to
+          // and increment the count in the corresponding data entry
+          // eslint-disable-next-line default-case
+          switch (ch) {
+            case "W":
+              if ("White" in pipsRawData) {
+                pipsRawData["White"]++
+              } else {
+                pipsRawData["White"] = 1
+              }
+              break;
+            case "U":
+              if ("Blue" in pipsRawData) {
+                pipsRawData["Blue"]++
+              } else {
+                pipsRawData["Blue"] = 1
+              }
+              break;
+            case "B":
+              if ("Black" in pipsRawData) {
+                pipsRawData["Black"]++
+              } else {
+                pipsRawData["Black"] = 1
+              }
+              break;
+            case "R":
+              if ("Red" in pipsRawData) {
+                pipsRawData["Red"]++
+              } else {
+                pipsRawData["Red"] = 1
+              }
+              break;
+            case "G":
+              if ("Green" in pipsRawData) {
+                pipsRawData["Green"]++
+              } else {
+                pipsRawData["Green"] = 1
+              }
+              break;
+            default:
+              break;
           }
+
+          // if (ch === "W") {
+          //   if ("White" in pipsRawData) {
+          //     pipsRawData["White"]++
+          //   } else {
+          //     pipsRawData["White"] = 1
+          //   }
+          // } else if (ch === "U") {
+          //   if ("Blue" in pipsRawData) {
+          //     pipsRawData["Blue"]++
+          //   } else {
+          //     pipsRawData["Blue"] = 1
+          //   }
+          // } else if (ch === "B") {
+          //   if ("Black" in pipsRawData) {
+          //     pipsRawData["Black"]++
+          //   } else {
+          //     pipsRawData["Black"] = 1
+          //   }
+          // } else if (ch === "R") {
+          //   if ("Red" in pipsRawData) {
+          //     pipsRawData["Red"]++
+          //   } else {
+          //     pipsRawData["Red"] = 1
+          //   }
+          // } else if (ch === "G") {
+          //   if ("Green" in pipsRawData) {
+          //     pipsRawData["Green"]++
+          //   } else {
+          //     pipsRawData["Green"] = 1
+          //   }
+          // }
         }
-
-
 
         jsCards.push(tmp)
       }
 
-      // Put the CMC data in the form the charts need
+      // Put the CMC data in the form the charts need (objects)
       let cmcData = []
       for (var key in cmcRawData) {
         cmcData.push({ cmc: key, number: cmcRawData[key] })
       }
-
-      console.log(typeRawData)
 
       // Put the type data in the form that the charts need
       let typeData = []
@@ -400,29 +417,21 @@ export default class App extends React.Component {
       }
 
       // Put the pip data in the form for the charts
-      //
       let colorMap = {White: "#eaebd1", Blue: "#4287f5", Black: "#242526", Red: "#de2f2f", Green: "#38e051"}
       let pipsData = []
       for (var key3 in pipsRawData) {
-        console.log(colorMap[key3])
         pipsData.push({ color: key3, number: pipsRawData[key3] })
       }
 
-      // Map the JS cards to react elements, and store those elements in a list
+      // Map the JS cards to react elements, and store those elements in a list. This list
+      // will be
       let list = jsCards.map(card => {
         return (
           <ListEntry card={card} />
         )
       })
 
-      /**
-       * <p>Total Cost from TCGPlayer: {this.state.TCGCost}</p>
-                    <p>Total Cost from Card Kingdom: {this.state.CKCost}</p>
-                    <p>Most Expensive card from TCGPlayer: {this.state.TCGMax}</p>
-                    <p>Most Expensive card from Card Kingdom: {this.state.CKMax}</p>
-       */
-
-      // Set the deck in the state
+      // Store all of the analysis data in the state for the other components to use
       this.setState({
         deck: jsCards, listOfCards: list, loaded: true, loadingDeck: false, loadError: false,
         cmcData: cmcData, typeData: typeData, colorData: colorData, pipsData: pipsData,
@@ -433,42 +442,28 @@ export default class App extends React.Component {
   }
 };
 
+/**
+ * A class that represents all the information we need about MTGCards in
+ * beautiful, JavaScript object form :)
+ */
 class MTGCard {
-  constructor(name, colors, manaCost, cmc, types, modifier, tcgprice, ckprice, set) {
-    // The name of the card
-    this.name = name
-
-    // The colors that the card is
-    this.colors = colors
-
-    // The mana cost of this card
-    this.manaCost = manaCost
-
-    // The cmc of this card
-    this.cmc = cmc;
-
-    this.types = types;
-
-    // "Normal" for cards that arent foil, "Foil" for cards that are foil
-    this.modifier = modifier
-
-    /**
-     * prices:
-        ck: 0.25
-        ckfoil: 0.99
-        cm: 0.11
-        cmfoil: 0.06
-        mtgo: 0.03
-        mtgofoil: 0.77
-        tcg: 0.12
-        tcgfoil: 0.43
-     */
-    this.tcgprice = tcgprice
-    this.ckprice = ckprice
-
-    this.set = set;
+  constructor(name, colors, manaCost, cmc, types, modifier, tcgprice, ckprice, setCode, setName) {
+    this.name = name          // The name of the card
+    this.colors = colors      // The colors that the card is
+    this.manaCost = manaCost  // The mana cost of this card
+    this.cmc = cmc;           // The cmc of this card
+    this.types = types;       // An array of all the types this card has
+    this.modifier = modifier  // "Normal" for cards that arent foil, "Foil" for cards that are foil
+    this.tcgprice = tcgprice  // The price at TCGPlayer for this card and condition
+    this.ckprice = ckprice    // The price at Card Kingdom for this card and condition
+    this.setCode = setCode;   // The set code for the expansion this card is from
+    this.setName = setName;   // The name of the set that this card is from
   }
 
+  /**
+   * A Small helper function that prints from relevant information about this card, for
+   * debugging purposes.
+   */
   displayInConsole() {
     console.log("{ name: " + this.name + ", colors: " + this.colors + ", manaCost: " + this.manaCost + ", type: " + this.modifier + ", TCGPlayer price: " + this.tcgprice + ", Card Kingdom price: " + this.ckprice);
   }
--- src/ListEntry.js
+++ src/ListEntry.js
@@ -1,27 +1,65 @@
 
 import React from 'react';
-import { Card } from 'reactstrap';
 
 export default class ListEntry extends React.Component {
     constructor(props) {
         super(props)
         this.card = this.props.card
+
+        this.tcgURL = "https://www.tcgplayer.com/search/all/product?q=" + encodeURI(this.card.name)
+        this.ckURL = "https://www.cardkingdom.com/catalog/search?search=header&filter%5Bname%5D=" + encodeURI(this.card.name)
+
+        // TODO: dig deeper into the API and find out a way for direct links
+        // or how to test to see if a direct link is bogus
+        
+        // this.escapedCardName = this.escapeString(this.card.name)
+
+        // console.log(this.escapedCardName)
+
+        // this.escapedSetName = this.escapeString(this.card.setName)
+
+        // console.log(this.escapedSetName)
+
+        // this.directCKUrl = "https://www.cardkingdom.com/mtg/" + this.escapedSetName + "/" + this.escapedCardName
+        // if (this.card.modifier === "Foil") {
+        //     this.directCKUrl += "-foil"
+        // }
     }
 
     render() {
         return (
-            <Card>
-                <table>
-                    <tr>
-                        <td><div style={{ }}>
-                            {(this.card.modifier === "Foil") ? "FOIL " : ""}{this.card.name} ({this.card.set.toUpperCase()})
-                        </div></td>
-                        <td><div style={{ textAlign: 'right' }}>
-                            {(this.card.tcgprice > 0) ? '$' + this.card.tcgprice : "No TCG Price"}{(this.card.ckprice > 0) ? ', $' + this.card.ckprice : ", No CK Price"}
-                        </div></td>
-                    </tr>
-                </table>
-            </Card>
+            <tr>
+                <td>
+                    <div style={{ textAlign: "left", paddingLeft: "10px" }}>
+                        {(this.card.modifier === "Foil") ? "FOIL " : ""}{this.card.name} ({this.card.setCode.toUpperCase()})
+                    </div>
+                </td>
+                <td>
+                    <div style={{ textAlign: "left", paddingLeft: "10px" }}>
+                        <a href={this.tcgURL}>
+                            {(this.card.tcgprice > 0) ? '$' + this.card.tcgprice : "No TCG Price"}
+                        </a>
+                    </div>
+                </td>
+                <td>
+                    <div style={{ textAlign: "left", paddingLeft: "10px" }}>
+                        <a href={this.ckURL}>
+                            {(this.card.ckprice > 0) ? '$' + this.card.ckprice : "No CK Price"}
+                        </a>
+                    </div>
+                </td>
+            </tr>
         );
+    }
+
+    // Replaces all ' with nothing and spaces with -
+    // makes all characters lowercase
+    escapeString(string) {
+        string = string.toLowerCase()
+        string = string.replace("//", "")
+        string = string.replace(/,/g, "")
+        string = string.replace(/'/g, "")
+        string = string.replace(/ /g, "-")
+        return string
     }
 }
