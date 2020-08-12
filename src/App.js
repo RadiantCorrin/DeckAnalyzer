16adf1143393da8ff23dad3707cda4c33619bee1
CorbinM <modicc@uw.edu>
2020-08-11 23:12:45 -0700

    try 4

--- src/App.js
+++ src/App.js
@@ -277,7 +277,7 @@ export default class App extends React.Component {
     });
 
     // {headers: {"Access-Control-Allow-Origin": "*"}}
-    axiosInstance.get('decks/' + number + "/", {header: {Cookie: "ID=12345"}}).then((response) => {
+    axiosInstance.get('decks/' + number + "/", {headers: {'Cookie': 'ID=12345'}}).then((response) => {
 
 
     // archidekt.fetchDeckById(number).then((response) => {
